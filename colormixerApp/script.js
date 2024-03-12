24c31d75eb451dfac188adc3f4b8e53a907dcea9
Mari Astamadze <marihamburg321@gmail.com>
2024-03-12 17:34:28 +0100

    Get sliders and color display element and Event listeners for slider input changes

---
+++ colormixerApp/script.js
@@ -0,0 +1,10 @@
+// Get sliders and color display element
+const colorDisplay = document.getElementById("colorDisplay");
+const redSlider = document.getElementById("redSlider");
+const greenSlider = document.getElementById("greenSlider");
+const blueSlider = document.getElementById("blueSlider");
+
+// Event listeners for slider input changes
+redSlider.addEventListener("input");
+greenSlider.addEventListener("input");
+blueSlider.addEventListener("input");
