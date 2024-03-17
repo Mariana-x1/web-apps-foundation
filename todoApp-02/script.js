4babec6ac65c8d797c3f5f46f0f92278ddbbd8b8
Mari Astamadze <marihamburg321@gmail.com>
2024-03-17 23:05:16 +0100

    add localstorage

--- todoApp-02/script.js
+++ todoApp-02/script.js
@@ -10,21 +10,31 @@ let todos = [
   { description: "learn CSS", done: false, id: 3 },
 ];
 
+// Load todos from Local Storage
+function loadTodosFromLocalStorage() {
+  const storedTodos = localStorage.getItem("todos");
+  if (storedTodos) {
+    todos = JSON.parse(storedTodos);
+  }
+}
+
+// Save todos to Local Storage
+function saveTodosToLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 // Event listener for submitting new todo
 todoForm.addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent form submission
   const todoText = newTodoInput.value.trim(); // Get the trimmed value from the input field
   // Check if the input field is not empty
   if (todoText !== "") {
-    /*  Generate a unique ID for the new todo - auch kann mann mit ternary Opperator
-    const newTodoId = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;*/
-
     // Create a new todo object
     const newTodo = {
       description: todoText,
       done: false,
       //Generate unique ID for the new todo
-      id: generateTodoId(), //generateId(), // newTodo (das kommt vom ternary operator)//Math.floor(Math.random() * 999999999 * Date.now()),
+      id: generateTodoId(),
     };
 
     // Add the new todo to the list of todos
@@ -34,6 +44,7 @@ todoForm.addEventListener("submit", function (event) {
     newTodoInput.value = "";
 
     // Render the updated todo list
+    saveTodosToLocalStorage();
     renderTodoList();
   }
 });
@@ -54,16 +65,7 @@ function renderTodoList() {
 let todoIdCounter = 1;
 function generateTodoId() {
   return Date.now();
+  saveTodosToLocalStorage();
 }
-/*
-function generateId() {
-  // Check if todos array is not empty
-  if (todos.length > 0) {
-    // If not empty, return ID of the last todo incremented by 1
-    return todos[todos.length - 1].id + 1;
-  } else {
-    // If empty, return 1 as the default ID
-    return 1;
-  }
-}*/
+loadTodosFromLocalStorage();
 renderTodoList();
